1eedead520aafcee9a898882a127abae34185174
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2025-10-17 06:07:49 +1030

    Delete two-wolves-portal directory

--- two-wolves-portal/src/app/auth/callback/route.ts
+++
@@ -1,5 +0,0 @@
-
-import { NextResponse } from 'next/server'
-export async function GET() {
-  return NextResponse.redirect(new URL('/post-auth', process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'))
-}
